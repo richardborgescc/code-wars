c531262070846595df9082045e2ae791bef8166a
Richard Borges <richard.borges@contaazul.com>
2021-03-18 16:28:23 -0300

    chore: add dart code and tests structure

--- dart/1-countTheSmileyFaces.dart
+++ dart/1-countTheSmileyFaces.dart
@@ -4,6 +4,23 @@
 
 import 'package:test/test.dart';
 
+void runTests() {
+  group("Fixed tests", () {
+    test("Testing for []", () => expect(countSmileys([]), equals(0)));
+    test("Testing for [':)', ';(', ';}', ':-D']", () => expect(countSmileys([':)', ';(', ';}', ':-D']), equals(2)));
+    test("Testing for [';]', ':[', ';*', ':\$', ';-D']", () => expect(countSmileys([';]', ':[', ';*', ':\$', ';-D']), equals(1)));
+    test("Testing for [':)',':(',':D',':O',':;']", () => expect(countSmileys([':)',':(',':D',':O',':;']), equals(2)));
+    test("Testing for [':-)',';~D',':-D',':_D']", () => expect(countSmileys([':-)',';~D',':-D',':_D']), equals(3)));
+    test("Testing for [':---)','))',';~~D',';D']", () => expect(countSmileys([':---)','))',';~~D',';D']), equals(1)));
+    test("Testing for [';~)',':)',':-)',':--)']", () => expect(countSmileys([';~)',':)',':-)',':--)']), equals(3)));
+    test("Testing for [':o)',':--D',';-~)']", () => expect(countSmileys([':o)',':--D',';-~)']), equals(0)));
+  });
+}
+
+void main() {
+  runTests();
+}
+
 bool isValidEyes(String eyes) {
   if (eyes == ':' || eyes == ';') {
     return true;
@@ -57,21 +74,4 @@ int countSmileys(List<String> arr) {
   }
   
   return count;
-}
-
-void runTests() {
-  group("Fixed tests", () {
-    test("Testing for []", () => expect(countSmileys([]), equals(0)));
-    test("Testing for [':)', ';(', ';}', ':-D']", () => expect(countSmileys([':)', ';(', ';}', ':-D']), equals(2)));
-    test("Testing for [';]', ':[', ';*', ':\$', ';-D']", () => expect(countSmileys([';]', ':[', ';*', ':\$', ';-D']), equals(1)));
-    test("Testing for [':)',':(',':D',':O',':;']", () => expect(countSmileys([':)',':(',':D',':O',':;']), equals(2)));
-    test("Testing for [':-)',';~D',':-D',':_D']", () => expect(countSmileys([':-)',';~D',':-D',':_D']), equals(3)));
-    test("Testing for [':---)','))',';~~D',';D']", () => expect(countSmileys([':---)','))',';~~D',';D']), equals(1)));
-    test("Testing for [';~)',':)',':-)',':--)']", () => expect(countSmileys([';~)',':)',':-)',':--)']), equals(3)));
-    test("Testing for [':o)',':--D',';-~)']", () => expect(countSmileys([':o)',':--D',';-~)']), equals(0)));
-  });
-}
-
-void main() {
-  runTests();
 }
---
+++ dart/2-sumArrays.dart
@@ -0,0 +1,23 @@
+/*
+  https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/dart
+*/
+
+import 'package:test/test.dart';
+
+void runTests() {
+  group("Fixed tests", () {
+    test("Testing for []", () => expect(sum([]), equals(0)));
+    test("Testing for [5]", () => expect(sum([5]), equals(5)));
+    test("Testing for [-5]", () => expect(sum([-5]), equals(-5)));
+    test("Testing for [1, 2, 3.4, 4.3]", () => expect(sum([1, 2, 3.4, 4.3]), equals(10.7)));
+    test("Testing for [1, -3, 2, 3, 4, -1]", () => expect(sum([1, -3, 2, 3, 4, -1]), equals(6)));
+  });
+}
+
+void main() {
+  runTests();
+}
+
+num sum(List<num> arr) {
+  return 0;
+}
