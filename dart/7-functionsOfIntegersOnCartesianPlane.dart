b22d0e045c461a72cad7fd6ca0572c7b99985cc2
Richard Borges <richard.borges@contaazul.com>
2021-03-29 17:24:02 -0300

    chore: solve integer functions cartesian plane problem

--- dart/7-functionsOfIntegersOnCartesianPlane.dart
+++ dart/7-functionsOfIntegersOnCartesianPlane.dart
@@ -8,32 +8,47 @@ import "package:test/test.dart";
 
 void main() {
   test("Basic-tests", () {
-    gridGenerator(8);
-    /*
     expect(sumin(5), equals(55));
     expect(sumax(8), equals(372));
     expect(sumsum(8), equals(576));
-    */
   });
 }
 
-int sumin(int n) {
-  // your code here
-}
-int sumax(int n) {
-  // your code here
-}
-int sumsum(int n) {
-  // your code here
+class Sums {
+  int sumMin = 0;
+  int sumMax = 0;
+  int sumSum = 0;
+
+  Sums(sumMin, sumMax, sumSum) {
+    this.sumMin = sumMin;
+    this.sumMax = sumMax;
+    this.sumSum = sumSum;
+  }
 }
 
-void gridGenerator(int n) {
+int sumin(int n) {
+  Sums sums = gridSums(n);
+  return sums.sumMin;
+}
+int sumax(int n) {
+  Sums sums = gridSums(n);
+  return sums.sumMax;
+}
+int sumsum(int n) {
+  Sums sums = gridSums(n);
+  return sums.sumSum;
+}
+
+Sums gridSums(int n) {
   int sumMin = 0;
   int sumMax = 0;
 
   for (int i = 0; i <= n; i++) {
     for (int j = 0; j <= n; j++) {
-      sumMax += max(i, j);
+      if (j < n) {
+        sumMax += max(i, j);
+      }
+
       sumMin += min(i, j);
     }
   }
@@ -42,4 +57,5 @@ void gridGenerator(int n) {
   print('sumMax: $sumMax');
   int sumSum = sumMax + sumMin;
   print('sumSum: $sumSum');
+  return new Sums(sumMin, sumMax, sumSum);
 }
